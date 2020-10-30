20b7a86e8140d02f938b17a9dd71448f6668d0cd
wernerkotze <wernerkotze.rsa@gmail.com>
2020-10-30 15:32:08 +0200

    Added config file

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ from datetime import datetime
 
 import os
 import traceback
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.cfg')
+
+# Airtable Variables 
+at_table_name = config.get('AIRTABLE', 'AIRTABLE_TABLE_NAME')
+at_base_id = config.get('AIRTABLE', 'AIRTABLE_BASE_ID')
+at_api_key = config.get('AIRTABLE', 'AIRTABLE_API_KEY')
 
 class Airtable:
-	def __init__(self, record_id, table_name=os.getenv("AIRTABLE_TABLE_NAME")):
+	def __init__(self, record_id, table_name = at_table_name):
 		self.record_id   = record_id
 		self.table_name  = table_name
 
 	def at_instance(self):
-		return Airtable(os.getenv("AIRTABLE_BASE_ID"), table_name, api_key=os.getenv('AIRTABLE_API_KEY'))
+		return Airtable(at_base_id, table_name, api_key=at_api_key)
 
 	def get_campaign_id(self):
-		record = airtable.get(self.record_id)
+		record = self.at_instance().get(self.record_id)
 
-# set variables
-campaign_id = ""
-record_id   = ""
+# # set variables
+# record_id  = "recDE4SwTI2fI9Lqh"
+# table_name= "Broad"
 
-p1 = Airtable(campaign_id, record_id)
+# p1 = Airtable(record_id)
 
-record = p1.get_record()
+# campaign_id = p1.get_campaign_id()
 
-print(f"Record: {record}")
+# print(f"Campaign ID: {campaign_id}")
+print(os.environ)
