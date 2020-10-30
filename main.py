2553c8277667ae8f1753487800d4eab2a94107cd
wernerkotze <wernerkotze.rsa@gmail.com>
2020-10-30 15:42:25 +0200

    Get campaign ID

--- main.py
+++ main.py
@@ -13,24 +13,22 @@ at_table_name = config.get('AIRTABLE', 'AIRTABLE_TABLE_NAME')
 at_base_id = config.get('AIRTABLE', 'AIRTABLE_BASE_ID')
 at_api_key = config.get('AIRTABLE', 'AIRTABLE_API_KEY')
 
-class Airtable:
+class airtableSync:
 	def __init__(self, record_id, table_name = at_table_name):
 		self.record_id   = record_id
 		self.table_name  = table_name
 
 	def at_instance(self):
-		return Airtable(at_base_id, table_name, api_key=at_api_key)
+		return Airtable(at_base_id, self.table_name, api_key=at_api_key)
 
 	def get_campaign_id(self):
-		record = self.at_instance().get(self.record_id)
+		return self.at_instance().get(self.record_id)['fields']['FB Campaign ID']
 
-# # set variables
-# record_id  = "recDE4SwTI2fI9Lqh"
-# table_name= "Broad"
+# set test variables
+record_id  = "recDE4SwTI2fI9Lqh"
 
-# p1 = Airtable(record_id)
+p1 = airtableSync(record_id, at_table_name)
 
-# campaign_id = p1.get_campaign_id()
+campaign_id = p1.get_campaign_id()
 
-# print(f"Campaign ID: {campaign_id}")
-print(os.environ)
+print(f"Campaign ID: {campaign_id}")
