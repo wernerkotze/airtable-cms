f4850a2343b99d21fd8a03e50d6014791772caba
wernerkotze <wernerkotze.rsa@gmail.com>
2020-10-30 22:40:42 +0200

    Started facebook integration

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from airtable import Airtable
 from facebook_business.api import FacebookAdsApi
-from facebook_business.adobjects.adcreative import AdCreative
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.adobjects.campaign import Campaign
 
 from datetime import datetime
 
 import traceback
 import configparser
+import requests
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
@@ -20,6 +22,10 @@ fb_app_id = config.get('FACEBOOK', 'APP_ID')
 fb_secret = config.get('FACEBOOK', 'APP_SECRET')
 fb_token = config.get('FACEBOOK', 'ACCESS_TOKEN')
 
+print(fb_app_id,
+fb_secret,
+fb_token)
+
 class airtableSync:
 	def __init__(self, record_id, table_name = at_table_name):
 		self.record_id   = record_id
@@ -28,14 +34,42 @@ class airtableSync:
 	def at_instance(self):
 		return Airtable(at_base_id, self.table_name, api_key=at_api_key)
 
-	def get_campaign_id(self):
-		return self.at_instance().get(self.record_id)['fields']['FB Campaign ID']
+	def fb_instance(self):
+		return FacebookAdsApi.init(fb_app_id, fb_secret, fb_token)
+
+	def get_adset_id(self):
+		return self.at_instance().get(self.record_id)['fields']['FB Adset ID']
+
+	def get_campaign_insights(self):
+		fb_instance = self.fb_instance()
+
+		print(fb_instance.__dict__)
+
+		adset_id = self.get_adset_id()
+
+		fields = [
+			'cost_per_conversion'
+		]
+
+		params = {
+		  'fields': fields,
+		  'access_token': fb_token
+		}
+
+		response = requests.get(f'https://graph.facebook.com/v8.0/23845695225790479/insights', params=params)
+		print(response.__dict__)
+
+		return response
+
 
 # set test variables
 record_id  = "recDE4SwTI2fI9Lqh"
 
 p1 = airtableSync(record_id, at_table_name)
 
-campaign_id = p1.get_campaign_id()
+campaign_id = p1.get_adset_id()
+campaign_insights = p1.get_campaign_insights()
 
-print(f"Campaign ID: {campaign_id}")
+print(f"Adset ID: {campaign_id}")
+print(f"Campaign Insights: {campaign_insights}")
+
