a876547b7284c62e8d81a95c39d5f188a00e22ed
wernerkotze <wernerkotze.rsa@gmail.com>
2022-04-10 13:36:38 +0200

    added quicket first response

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ import traceback
 import configparser
 import requests
 
+
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
-# Airtable Variables
+# Airtable 
 at_table_name = config.get('AIRTABLE', 'TABLE_NAME')
 at_base_id = config.get('AIRTABLE', 'BASE_ID')
 at_api_key = config.get('AIRTABLE', 'API_KEY')
+
+# Quicket 
+quicket_base_url = config.get('QUICKET', 'BASE_URL')
+quicket_api_key = config.get('QUICKET', 'API_KEY')
+
+today = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 
 class airtableSync:
 	def __init__(self, record_id, table_name = at_table_name):
@@ -22,8 +29,35 @@ class airtableSync:
 	def at_instance(self):
 		return Airtable(at_base_id, self.table_name, api_key=at_api_key)
 
-table = Table(at_api_key, at_base_id, at_table_name)
-results = table.all()
 
-print(results)
+class Quicket:
+	def __init__(self, base_url = quicket_base_url, api_key = quicket_api_key):
+		self.base_url = base_url
+		self.api_key  = api_key
+
+	def get_events(self, pageSize, lastModified = today):
+		url = f"{self.base_url}?api_key={self.api_key}&pageSize={pageSize}&page=1&lastModified={lastModified}"
+
+		payload = {}
+		headers = {}
+
+		response = requests.request("GET", url, headers=headers, data=payload)
+
+		return response.text
+
+
+# Quicket tets
+
+p1 = Quicket(quicket_base_url, quicket_api_key)
+
+results = p1.get_events(10)
+
+print(f"Events: {results}")
+
+# AT test
+
+# table = Table(at_api_key, at_base_id, at_table_name)
+# results = table.all()
+
+# print(results)
 
